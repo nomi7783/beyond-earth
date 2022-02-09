6ec61cfb1f1a628ba133b4a206a39801725bfb01
NOMAN_7783 <noman.khan@o3interfaces.com>
2022-02-09 16:54:21 +0500

    All work added

---
+++ Our Team/ourTeam.vue
@@ -0,0 +1,268 @@
+<template>
+    <section>
+        <h1>Our Team</h1>
+
+        <!--row-1-->
+    <div class="player-section">
+        <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-1.png" alt="player-img-1">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>  
+      
+       <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-2.png" alt="player-img-2">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div> 
+
+         <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-3.png" alt="player-img-3">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div> 
+
+         <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-4.png" alt="player-img-4">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div> 
+        
+       
+        </div>
+
+
+      <!--row-1-->
+    <div class="player-section">
+
+       <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-5.png" alt="player-img-5">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div> 
+
+         <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-6.png" alt="player-img-6">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div> 
+
+         <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-7.png" alt="player-img-7">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div> 
+
+         <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-8.png" alt="player-img-8">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>   
+        
+       
+        </div>
+        
+    </section>   
+</template>
+
+<script>
+export default {
+    name:'OurTeam'
+}
+</script>
+
+<style lang="scss" scoped>
+
+ section{
+        //    display: flex;
+        //    flex-direction: column;
+        //    justify-content: center;
+        //    align-items: center;
+        padding: 70px 0px 0px;
+          background: #081519 ;
+          background-image: url("../assets/imgs/our-team-img/our-team-background.png") , linear-gradient(360deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
+        //   background-color: linear-gradient(360deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
+           background-size: cover;
+           background-position: center;
+          height:auto;
+        //    min-height:90vh;
+
+             h1{
+                font-size: 60px;
+                font-style: bold;
+                color: rgba($color: #ffffff, $alpha: 1.0);
+                position: relative;
+                text-align: center;
+                max-width: 18rem;
+                margin: 0 auto;
+            }
+          
+            h1::after{
+                content:"";
+                position: absolute;
+                top: 100%;
+                left: -30px;
+                width: 100vw;
+                background-image: url("../assets/imgs/coreFeatures/line.png") ;
+                object-fit: cover;
+                background-repeat: no-repeat;
+                height: 20px;
+                margin-bottom:20px;
+                text-align: center;
+               
+            }
+             
+           }
+          
+
+.player-section{
+    display:flex;
+    justify-content:space-between;
+
+}
+
+.player-section:first-of-type{
+    margin:100px 0;
+}
+
+
+.player-details {
+        max-width: 273px;
+    width: 100%;
+//     margin: 0 20px;
+
+//     :first-of-type {
+//     margin: 0px;
+// }
+
+// :last-of-type {
+//     margin: 0px;
+// }
+
+.player-img{
+        position:relative;
+        padding:0px;
+    img{
+    width:100%;
+
+// background: linear-gradient(360deg, #E99300 -20%, rgba(0, 0, 0, 0) 92%);
+}
+
+img:last-of-type{
+    position:absolute;
+    bottom:-30px;
+    left:0px;
+}
+
+}
+
+
+
+
+
+
+ 
+
+
+div{
+        background: #08040A;
+    text-align:center;
+    padding: 49px 0 49px;
+        margin-top: -10px;
+
+  h3 {
+    color: #fff;
+    margin-bottom: 20px;
+    }
+
+ p {
+    color: #DE9635;
+    font-size: 14px;
+}
+}
+
+
+
+}
+
+  
+
+
+@media (max-width: 1140px) {
+    .player-details {
+    max-width: 230px;
+
+}
+
+}
+
+
+@media (max-width: 992px) {
+    .player-details {
+    max-width: 180px;
+
+.player-img {
+
+    img:last-of-type{
+
+    bottom: -17px;
+
+    }
+
+}
+
+    div{
+
+            padding: 25px 0 25px;
+        h3{
+            margin-bottom: 5px;
+    }
+    }
+
+}
+
+}
+
+
+
+</style>
---
+++ footer/footer.vue
@@ -0,0 +1,138 @@
+<template>
+
+
+
+ 
+     <footer>
+        <a href="#">
+        <img src="../assets/imgs/footer-images/footer-logo.png" alt="footer-logo">
+    </a>
+        <div class="social-icons">
+          
+
+            <a href="#">
+                <img src="../assets/imgs/footer-images/footer-social-1.png" alt="footer-social-1">
+            </a>
+            <a href="#">
+                <img src="../assets/imgs/footer-images/footer-social-2.png" alt="footer-social-2">
+            </a>
+            <a href="#">
+                <img src="../assets/imgs/footer-images/footer-social-3.png" alt="footer-social-3">
+            </a>
+            <a href="#">
+                <img src="../assets/imgs/footer-images/footer-social-4.png" alt="footer-social-4">
+            </a>
+        </div>
+
+        <p>BEO has traditional elements of roleplay and exploration the MMO’s we love have, combined with the play to earn mechanics of blockchain technology making it the first of its kind.</p>
+
+        
+        <div class="sub-footer">
+            <p>2021 BeyondEarth. All Rights Reserved</p>
+            <div>
+                <a href="#">Privacy Notice</a>
+                <p>|</p>
+                <a href="#">Cookie Notice</a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style >
+        footer {
+    background-color: #000;
+    text-align: center;
+    padding: 52px 100px 30px;
+}
+
+.social-icons {
+    max-width: 190px;
+    margin: 36px auto 20px;
+    display: flex;
+    justify-content: space-between;
+    
+}
+
+footer>img {
+    max-width: 120px;
+    width: 100%;
+}
+
+footer>p {
+    font-family: poppins ,sans-serif;
+    max-width: 760px;
+    margin: 0 auto;
+    line-height: 24px;
+    margin-bottom: 60px;
+    color: white;
+    font-size: 16px;
+}
+
+
+.sub-footer {
+    border: 1px soild red !important;
+    border-top: 1px solid #808080;
+    padding-top: 32px;
+}
+
+
+.sub-footer p, a {
+    color: #808080;
+    font-family: 'Poppins';
+    text-decoration: none;
+    font-size: 16px;
+}
+
+.sub-footer {
+    display: flex;
+    justify-content: space-between;
+}
+
+.sub-footer div {
+    display: flex;
+    max-width: 250px;
+    justify-content: space-between;
+    width: 100%;
+}
+
+
+
+/* ======media-query============ */
+@media (max-width: 768px) {
+    footer {
+    padding: 52px 18px 30px;
+}
+}
+
+@media (max-width: 580px) {
+    .sub-footer {
+    flex-direction: column;
+    align-items: center;
+
+}
+
+.sub-footer div {
+    flex-direction: column;
+}
+
+.sub-footer p, a {
+    margin-bottom: 14px;
+    font-size: 14px;
+}
+
+.sub-footer div p:first-of-type {
+    display: none;
+}
+footer>p {
+    font-size: 14px;
+}
+
+}
+
+</style>
