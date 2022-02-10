a9c75da3b22dd1abc173f6882fded3b11025c5ac
NOMAN_7783 <noman.khan@o3interfaces.com>
2022-02-10 16:14:59 +0500

    our team updated file

--- Our Team/ourTeam.vue
+++ Our Team/ourTeam.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <h1>Our Team</h1>
+      <h1>Our Team</h1> 
 
         <!--row-1-->
     <div class="player-section">
@@ -101,129 +101,231 @@
         
        
         </div>
+
+
+<div class="slick-row">
+        <Slick ref="slick" :options="slickOptions" style="overflow:hidden;" >
+  
+  <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-8.png" alt="player-img-8">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>  
+        <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-8.png" alt="player-img-8">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>  
+        <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-8.png" alt="player-img-8">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>  
+        <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-8.png" alt="player-img-8">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>  
+        <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-8.png" alt="player-img-8">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>  
+        <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-8.png" alt="player-img-8">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>  
+        <div class="player-details">
+        <div class="player-img">
+              <img src="../assets/imgs/our-team-img/player-img-8.png" alt="player-img-8">
+              <img src="../assets/imgs/our-team-img/player-before-img.png" alt="player-before-img">
+              </div>
+              <div>
+              <h3>CARLOS</h3>
+              <p>FRONT END DEV</p>
+              </div>
+        </div>  
+        </Slick>
+        </div>
         
     </section>   
 </template>
 
+
+
 <script>
+import Slick from 'vue-slick';
 export default {
+    components: {Slick},
+        data() {
+        return {
+            slickOptions: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots: false,
+                arrows:false,
+                infinite: false,
+                centerMode: true,
+                infinite:true,
+                centerPadding: '0px',
+                
+
+    responsive: [
+    {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: '97px',
+      }
+    },
+      {
+      breakpoint: 420,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: '63px',
+      }
+    }
+   
+
+  ]
+            },
+        };
+    },
     name:'OurTeam'
 }
 </script>
 
-<style lang="scss" scoped>
-
- section{
-        //    display: flex;
-        //    flex-direction: column;
-        //    justify-content: center;
-        //    align-items: center;
-        padding: 70px 0px 0px;
-          background: #081519 ;
-          background-image: url("../assets/imgs/our-team-img/our-team-background.png") , linear-gradient(360deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
-        //   background-color: linear-gradient(360deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
-           background-size: cover;
-           background-position: center;
-          height:auto;
-        //    min-height:90vh;
-
-             h1{
-                font-size: 60px;
-                font-style: bold;
-                color: rgba($color: #ffffff, $alpha: 1.0);
-                position: relative;
-                text-align: center;
-                max-width: 18rem;
-                margin: 0 auto;
-            }
-          
-            h1::after{
-                content:"";
-                position: absolute;
-                top: 100%;
-                left: -30px;
-                width: 100vw;
-                background-image: url("../assets/imgs/coreFeatures/line.png") ;
-                object-fit: cover;
-                background-repeat: no-repeat;
-                height: 20px;
-                margin-bottom:20px;
-                text-align: center;
-               
-            }
-             
-           }
-          
-
-.player-section{
-    display:flex;
-    justify-content:space-between;
-
-}
-
-.player-section:first-of-type{
-    margin:100px 0;
-}
 
 
-.player-details {
-        max-width: 273px;
-    width: 100%;
-//     margin: 0 20px;
+<style>
 
-//     :first-of-type {
-//     margin: 0px;
-// }
-
-// :last-of-type {
-//     margin: 0px;
-// }
-
-.player-img{
-        position:relative;
-        padding:0px;
-    img{
-    width:100%;
-
-// background: linear-gradient(360deg, #E99300 -20%, rgba(0, 0, 0, 0) 92%);
-}
-
-img:last-of-type{
-    position:absolute;
-    bottom:-30px;
-    left:0px;
-}
-
-}
-
-
-
-
-
-
+@import "node_modules/slick-carousel/slick/slick.css";
  
-
-
-div{
-        background: #08040A;
-    text-align:center;
-    padding: 49px 0 49px;
-        margin-top: -10px;
-
-  h3 {
-    color: #fff;
-    margin-bottom: 20px;
-    }
-
- p {
-    color: #DE9635;
-    font-size: 14px;
+ section {
+	 padding: 70px 0px 20px;
+	 background: #081519;
+	 background-image: url("../assets/imgs/our-team-img/our-team-background.png"), linear-gradient(360deg, #000 0%, rgba(0, 0, 0, 0) 100%);
+	 background-size: cover;
+	 background-position: center;
+	 height: auto;
 }
+ section h1 {
+	 font-size: 60px;
+	 font-style: bold;
+	 color: rgba(255, 255, 255, 1.0);
+	 position: relative;
+	 text-align: center;
+	 max-width: 18rem;
+	 margin: 0 auto;
+}
+ section h1::after {
+	 content: "";
+	 position: absolute;
+	 top: 100%;
+	 left: -30px;
+	 width: 100%;
+	 background-image: url("../assets/imgs/coreFeatures/line.png");
+	 object-fit: cover;
+	 background-repeat: no-repeat;
+	 height: 20px;
+	 margin-bottom: 20px;
+	 text-align: center;
+}
+ .player-section {
+	 display: flex;
+	 justify-content: space-between;
+}
+.player-section:first-of-type {
+	 margin: 100px 0;
+}
+ .player-details {
+	 max-width: 273px;
+	 width: 100%;
+}
+ .player-details .player-img {
+	 position: relative;
+	 padding: 0px;
+}
+ .player-details .player-img img {
+	 width: 100%;
+}
+ .player-details .player-img img:last-of-type {
+	 position: absolute;
+	 bottom: -30px;
+	 left: 0px;
+}
+ .player-details div {
+	 background: #08040a;
+	 text-align: center;
+	 padding: 49px 0 49px;
+	 margin-top: -10px;
+}
+ .player-details div h3 {
+	 color: #fff;
+	 margin-bottom: 20px;
+}
+ .player-details div p {
+	 color: #de9635;
+	 font-size: 14px;
+}
+
+.slick-row .slick-slide {
+    display: flex;
+    justify-content: center;
+}
+
+.slick-row{
+    display: none;
+}
+
+.slick-row .slick-track{
+    display: flex;
+    justify-content: center;
+    align-content: center;
+}
+
+
+.slick-slide.slick-current.slick-active.slick-center .player-details {
+    max-width: 200px;
 }
 
 
 
-}
+
+
 
   
 
@@ -233,35 +335,63 @@ div{
     max-width: 230px;
 
 }
-
 }
 
 
 @media (max-width: 992px) {
-    .player-details {
-    max-width: 180px;
+	 .player-details {
+		 max-width: 180px;
+	}
+	 .player-details .player-img img:last-of-type {
+		 bottom: -25px;
+	}
+	 .player-details div {
+		 padding: 50px 0 25px;
+	}
+	 .player-details div h3 {
+		 margin-bottom: 5px;
+	}
+}
+ 
 
-.player-img {
+@media (max-width: 768px) {
+section .player-section .player-details {
+    display: none;
+}
+ .player-details {
+		 max-width: 140px;
+	}
 
-    img:last-of-type{
+.slick-row{
+    display: block;
+}
 
-    bottom: -17px;
+.player-section:first-of-type {
+    margin: 40px 0;
+}
 
-    }
 
 }
 
-    div{
 
-            padding: 25px 0 25px;
-        h3{
-            margin-bottom: 5px;
-    }
-    }
 
+@media (max-width: 580px) {
+    section h1 {
+        font-size: 28px;
+    }
+ section h1::after {
+	 background-image: url("../assets/imgs/our-team-img/our-team-line.png");
+         left: 62px;
+         width:10rem;
+ }
+
+ .player-section:first-of-type {
+    margin: 25px 0;
 }
 
 }
+
+
 
 
 
